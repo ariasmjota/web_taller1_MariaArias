e9723149d58ac285e93e8cd5630afc7281c44826
unknown <majobel10@hotmail.com>
2019-09-09 00:51:41 -0500

    bye gallery

--- scrip/java.js
+++ scrip/java.js
@@ -1,30 +1,25 @@
 
     var numImagen = 1;
-var anterior=document.querySelector(".galeria__anterior");
-var siguiente=document.querySelector(".galeria__siguiente");
+var anterior=document.querySelector(".galeria__back");
+var siguiente=document.querySelector(".galeria__next");
 var p1=document.querySelector(".p1");
 var p2=document.querySelector(".p2");
 var p3=document.querySelector(".p3");
+var p3=document.querySelector(".p4");
 mostrarImagenes(numImagen);
 
 function otraImagen(n) {
     mostrarImagenes(numImagen += n);
 }
 
-/*anterior.addEventListener('click',otraImagen(-1));
-siguiente.addEventListener('click',otraImagen(1));*/
-
 function imagenActual(n) {
     mostrarImagenes(numImagen = n);
 }
-/*p1.addEventListener('click',imagenActual(1));
-p2.addEventListener('click',imagenActual(2));
-p3.addEventListener('click',imagenActual(3));*/
 
 function mostrarImagenes(n) {
   var i;
-  var slides = document.querySelectorAll(".galeria__imagen");
-  var puntos = document.querySelectorAll(".galeria__punto");
+  var slides = document.querySelectorAll(".gallery__imagen");
+  var puntos = document.querySelectorAll(".gallery__punto");
 
   if (n > slides.length) {numImagen = 1}    
   if (n < 1) {numImagen = slides.length}
